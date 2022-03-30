5d75ee64af774c4d33bae417e66dc022079a4b7d
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-30 09:06:44 +0530

    Some Completed

---
+++ components/Utils/PopUpButton.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const PopUpButton = () => {
+    return (
+        <button
+            // type={props.type}
+            className="h-[29px] w-[105px] rounded-full bg-gradient-to-r from-pink to-gradient_yellow px-4 text-lg font-bold text-white sm:h-[37px] sm:w-[133px] text-center"
+        >
+            {/* {props.label} */}
+            Proceed
+        </button>
+    )
+}
+
+export default PopUpButton;
